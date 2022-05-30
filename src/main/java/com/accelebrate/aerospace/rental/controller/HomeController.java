c0c7a15ac6ae2d00814fb9f626b195476c5a9343
wyattlang <wyattlang2001@gmail.com>
2022-05-30 15:13:58 -0400

    initial commit

--- src/main/java/com/accelebrate/aerospace/rental/controller/HomeController.java
+++ src/main/java/com/accelebrate/aerospace/rental/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.accelebrate.aerospace.rental.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
 public class HomeController {
 
+	@Value("${cars.title}")
+	private String appTitle;
+	
 	@GetMapping
-	public String goHome() {
+	public String goHome(Model model) {
+		model.addAttribute("title", appTitle);
 		return "home";
 	}
 	
