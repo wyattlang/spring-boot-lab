d7270c9c11f9aa3475126048e9ac422c6f045725
wyattlang <wyattlang2001@gmail.com>
2022-05-30 08:59:21 -0400

    initial commit

---
+++ src/test/java/com/accelebrate/aerospace/rental/RentalCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accelebrate.aerospace.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
