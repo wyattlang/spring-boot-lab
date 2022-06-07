1e9c2648cb4b2ab6f3dd68718cba33014b0baabb
wyattlang <wyattlang2001@gmail.com>
2022-06-06 23:45:53 -0400

    initial commit

--- src/main/java/com/accelebrate/aerospace/rental/controller/HomeController.java
+++ src/main/java/com/accelebrate/aerospace/rental/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.accelebrate.aerospace.rental.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,13 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
 public class HomeController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
 
 	@Value("${cars.title}")
 	private String appTitle;
 	
 	@GetMapping
 	public String goHome(Model model) {
-		model.addAttribute("title", appTitle);
+		try {
+			model.addAttribute("title", appTitle);
+		} catch (IllegalArgumentException e) {
+			LOG.error("null attributeName passed to org.springframework.ui.Model's addAttribute() method");
+			throw new IllegalArgumentException();
+		}
+		
+		LOG.info("serving up home.html");
+		
 		return "home";
 	}
 	
