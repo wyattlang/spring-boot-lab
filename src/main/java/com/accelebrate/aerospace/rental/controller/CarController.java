c1f26fb343d0e3c287b5b58d723255aed3d9dd67
wyattlang <wyattlang2001@gmail.com>
2022-06-08 10:24:39 -0400

    initial commit

--- src/main/java/com/accelebrate/aerospace/rental/controller/CarController.java
+++ src/main/java/com/accelebrate/aerospace/rental/controller/CarController.java
@@ -1,13 +1,20 @@
 package com.accelebrate.aerospace.rental.controller;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.accelebrate.aerospace.rental.config.RentalProperties;
+import com.accelebrate.aerospace.rental.config.RentalProperties.Airport;
 import com.accelebrate.aerospace.rental.dao.CarService;
 import com.accelebrate.aerospace.rental.dao.RentalCar;
 
@@ -17,12 +24,30 @@ public class CarController {
 
 	@Autowired
 	private CarService carService;
+	@Autowired
+	private RentalProperties rentalProperties;
 	
 	@GetMapping
-	public String getCars(Model model) {
-		Collection<RentalCar> allCars = carService.getAll();
+	public String getCars(Model model, @RequestParam("code") Optional<String> optionalString) {
+		String code = optionalString.orElse("ORD");
+		Collection<RentalCar> allCars = carService.getAll().stream()
+				.filter(p -> p.getLocation().equals(code))
+				.collect(Collectors.toList());
 		model.addAttribute("cars", allCars);
 		return "car";
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseBody
+	public RentalCar getIndividualCar(@PathVariable("id") Long id) {
+		RentalCar car = carService.getByID(id);
+		Airport airport = rentalProperties.getAirports().stream()
+				.filter(p -> p.getIataCode().equals(car.getLocation()))
+				.findFirst()
+				.get();
+		double tax = car.getDailyRate() * airport.getTax() / 100;
+		car.setDailyRate(car.getDailyRate() + tax);
+		return car;
 	}
 	
 }
